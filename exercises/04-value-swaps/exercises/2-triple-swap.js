b64bc633d76e98a735a8f7f856a4c444b6fb6224
Mamé <mameazad.turgut@gmail.com>
2020-06-16 13:31:58 +0200

    solve part 04

--- exercises/04-value-swaps/exercises/2-triple-swap.js
+++ exercises/04-value-swaps/exercises/2-triple-swap.js
@@ -6,9 +6,10 @@ let c = 2;
 let temp = null;
 
 // write some code!
-
-
-
+temp = a;
+a = b;
+b = c;
+c = temp;
 // fill in the _ to pass the assertions
 
 const isTrue1 = a === 1;
@@ -20,6 +21,5 @@ console.assert(isTrue2, 'Test 2');
 const isTrue3 = c === 3;
 console.assert(isTrue3, 'Test 3');
 
-const isTrue4 = temp === _;
+const isTrue4 = temp === 3;
 console.assert(isTrue4, 'Test 4');
-
--- exercises/04-value-swaps/exercises/6-let-and-const.js
+++ exercises/04-value-swaps/exercises/6-let-and-const.js
@@ -12,21 +12,18 @@ temp = a;
 const c = temp;
 
 // write your code below here, and above the assertions
-
-
-
-
+a = b;
+b = temp;
 // replace the _ with a value to pass the assertions
 
 const isTrue1 = a === 1;
-console.assert(isTrue1, "Test 1");
+console.assert(isTrue1, 'Test 1');
 
 const isTrue2 = b === 2;
-console.assert(isTrue2, "Test 2");
+console.assert(isTrue2, 'Test 2');
 
-const isTrue3 = c === _;
-console.assert(isTrue3, "Test 3");
+const isTrue3 = c === temp;
+console.assert(isTrue3, 'Test 3');
 
-const isTrue4 = temp === _;
-console.assert(isTrue4, "Test 4");
-
+const isTrue4 = temp === c;
+console.assert(isTrue4, 'Test 4');
