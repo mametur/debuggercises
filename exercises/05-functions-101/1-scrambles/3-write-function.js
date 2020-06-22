624254b756ce1bb7e17a13cbf4d95ff1f013d94e
Mamé <mameazad.turgut@gmail.com>
2020-06-22 13:10:32 +0200

    solve part 7

--- exercises/05-functions-101/1-scrambles/3-write-function.js
+++ exercises/05-functions-101/1-scrambles/3-write-function.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function mystery(a, b) {
 	if (Boolean(a) === false && Boolean(b) === false) {
 		return 'path 1';
@@ -10,31 +9,12 @@ function mystery(a, b) {
 }
 const _1_expect = 'path 1';
 const _1_actual = mystery(0, null);
-||||||| merged common ancestors
-function mystery(a, b) {
-
-}
-
-
-const _1_expect = 'path 1';
-const _1_actual = mystery(0, null);
-=======
-'use strict';
-
-const scramble = () => {
-
-};
-
-const _1_expect = "yxz";
-const _1_actual = scramble('x', 'z', 'y');
->>>>>>> d7a0df91cdbeaabba9264cb3ac7fe6f23377b6f6
 console.assert(_1_actual === _1_expect, 'Test 1');
 
-const _2_expect = "zxy";
+const _2_expect = 'zxy';
 const _2_actual = scramble('x', 'y', 'z');
 console.assert(_2_actual === _2_expect, 'Test 2');
 
-const _3_expect = "zxy";
+const _3_expect = 'zxy';
 const _3_actual = scramble('x', 'z', 'y');
 console.assert(_3_actual === _3_expect, 'Test 3');
-
