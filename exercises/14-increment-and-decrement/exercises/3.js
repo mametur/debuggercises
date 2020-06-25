676c21ac42413c78f1aef16fd6ff8204d0340761
Mamé <mameazad.turgut@gmail.com>
2020-06-25 13:51:25 +0200

    solve part 14

--- exercises/14-increment-and-decrement/exercises/3.js
+++ exercises/14-increment-and-decrement/exercises/3.js
@@ -6,16 +6,16 @@ let y = x--;
 console.assert(x === -1, 'Test 1 x');
 console.assert(y === 0, 'Test 1 y');
 
-x = _;
+x = y--; // y = 0
 console.assert(x === 0, 'Test 2 x');
 console.assert(y === -1, 'Test 2 y');
 
-let z = _;
+let z = x++; // y -1
 console.assert(x === 1, 'Test 3 x');
 console.assert(y === -1, 'Test 3 y');
 console.assert(z === 0, 'Test 3 z');
 
-_ = --z;
+y = --z; // z 0
 console.assert(x === 1, 'Test 4 x');
 console.assert(y === -1, 'Test 4 y');
 console.assert(z === -1, 'Test 4 z');
